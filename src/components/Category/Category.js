9025aa97fb76fec6c1bd952b251af77a8592db62
rainbowdev <rainbowskyworks@gmail.com>
2023-04-13 10:13:30 +1000

    Routes defined and extra components added.  Category, SubCategory and Home components - initial implementation. Needs debugging and more functionality.

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Stack, Card, CardActionArea, CardMedia, CardContent, Typography } from '@mui/material'
+import data from '../../data/data.json'
+const Category = ({ category, handleClick }) => {
 
-const Category = () => {
-  return (
-    <div>Category</div>
-  )
+    return (
+        <>
+            <h2>{category.toUpperCase()}</h2>
+            <Stack direction={'row'}>
+                
+                {
+                    data.categories[category].map( cat => {
+                        const {id, name, description, image, subcategory} = cat
+                        return (
+                            <Card key={id} sx={{ maxWidth: 345 }}>
+                                <CardActionArea onClick={handleClick}>
+                                    <CardMedia
+                                    id={subcategory}
+                                    component="img"
+                                    height="140"
+                                    image={image}
+                                    alt={name}
+                                    />
+                                    <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {subcategory}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {description}
+                                    </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        )  
+                    })
+                }
+            </Stack>
+        </>
+    )
 }
 
 export default Category
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 import AdbIcon from '@mui/icons-material/Adb';
+import { Stack } from '@mui/system';
 //need Context API to hold signin/signoff status, loading
 const NavBar = () => {
   return (
     <div>
-        <AdbIcon />
+        <Stack style={{marginLeft: '10px', marginTop: '10px'}}>
+            <AdbIcon fontSize="large" color="primary" />
+            <p>Bee Boutique</p>
+        </Stack>
         <nav className='nav-bar'>
             <NavLink to="/" className={({ isActive, isPending }) =>
                 isPending ? "pending" : isActive ? "active" : ""
