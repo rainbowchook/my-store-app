078c5121b6e54ed8651d18e78c657f292075efb1
rainbowdev <rainbowskyworks@gmail.com>
2023-04-13 10:12:11 +1000

    created initial app structure with main pages/components; installed material UI comonent library for styling

---
+++ src/components/App/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import NavBar from '../NavBar/NavBar';
+import Home from '../Home/Home';
+import Category from '../Category/Category';
+import SignIn from '../SignIn/SignIn';
+import Register from '../Register/Register';
+import NotFound from '../NotFound/NotFound';
+import Checkout from '../Checkout/Checkout';
+import Profile from '../Profile/Profile'
+import Footer from '../Footer/Footer';
+import { CssBaseline } from '@mui/material';
+
+function App() {
+  return (
+    <>
+      <CssBaseline enableColorScheme/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route index path="/" element={<Home />} />
+          <Route exact path="signin" element={<SignIn />} />
+          <Route exact path="register" element={<Register />} />
+          <Route exact path=":category" element={<Category />} />
+          <Route exact path="checkout" element={<Checkout />} />
+          <Route exact path="profile/:user" element={<Profile />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
